15f4a1fd905ff168cede4f879473bba61a862b2d
Luchs <github@meisterluchs.de>
2019-06-30 22:51:26 +0200

    some button changes

--- app/src/main/java/de/meisterluchs/moviebingo/MainActivity.java
+++ app/src/main/java/de/meisterluchs/moviebingo/MainActivity.java
@@ -3,6 +3,8 @@ package de.meisterluchs.moviebingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private Button B1 = findViewById(R.id.B1);
+//    private Button I1 = findViewById(R.id.I1);
+//    private Button N1 = findViewById(R.id.N1);
+//    private Button G1 = findViewById(R.id.G1);
+//    private Button O1 = findViewById(R.id.O1);
+//    private Button B2 = findViewById(R.id.B2);
+//    private Button I2 = findViewById(R.id.I2);
+//    private Button N2 = findViewById(R.id.N2);
+//    private Button G2 = findViewById(R.id.G2);
+//    private Button O2 = findViewById(R.id.O2);
+//    private Button B3 = findViewById(R.id.B3);
+//    private Button I3 = findViewById(R.id.I3);
+//    private Button G3 = findViewById(R.id.G3);
+//    private Button O3 = findViewById(R.id.O3);
+//    private Button B4 = findViewById(R.id.B4);
+//    private Button I4 = findViewById(R.id.I4);
+//    private Button N4 = findViewById(R.id.N4);
+//    private Button G4 = findViewById(R.id.G4);
+//    private Button O4 = findViewById(R.id.O4);
+//    private Button B5 = findViewById(R.id.B5);
+//    private Button I5 = findViewById(R.id.I5);
+//    private Button N5 = findViewById(R.id.N5);
+//    private Button G5 = findViewById(R.id.G5);
+//    private Button O5 = findViewById(R.id.O5);
+
+//    // called when button is pressed
+//    public void tapButton(View activity_main) {
+//        B1.setBackgroundColor(1);
+//    }
 }
